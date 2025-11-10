ba7d67b19e3dd93ea3ab7d5f4db4b1c6ef0ca939
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-11-10 13:00:41 +0700

    remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-domain.com', // Replace with your domain
+  site: 'https://muhhae.github.io', // Replace with your domain
   devToolbar: {
     enabled: false
   },
