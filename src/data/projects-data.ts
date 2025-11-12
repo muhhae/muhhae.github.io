f2409dfe54df77595cd5eed3d0fe1df59b1dd692
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-11-12 23:29:40 +0700

    update

--- src/data/projects-data.ts
+++ src/data/projects-data.ts
@@ -16,6 +16,7 @@ export const projectsData: Project[] = [
     title: "Interpreted Programming Language",
     description: "Implemented core programming language features like <strong>variables, arithmetic, functions, and classes</strong>. Designed and implemented a <strong>custom IDE</strong> with syntax highlighting and an interactive shell.",
     link: "https://github.com/muhhae/interpreted-lang-py",
+    images: ["/screenshots/pyhaekal.png"],
     featured: false,
   },
   {
@@ -23,18 +24,21 @@ export const projectsData: Project[] = [
     description: "Implemented the <strong>game mechanics and 2D collision detection</strong> from scratch using C++.",
     link: "https://github.com/muhhae/tetromania-castle",
     web: "https://muhhae.itch.io/tetromania-castle",
+    images: ["/screenshots/tetromania.gif"],
     featured: false,
   },
   {
     title: "ESP32 PingPong Game",
     description: "A Ping-Pong game written in C++ for ESP32 and a dot-matrix display. Features <strong>dot-matrix display rendering</strong> and a <strong>buzzer-based music player</strong>.",
     link: "https://github.com/muhhae/esp32-pingpong",
+    images: ["/screenshots/esp32pong.jpeg"],
     featured: false,
   },
   {
     title: "Image sharing platform - Lorem Ipsum",
     description: "Developed a web application for sharing random images using Go and HTMX. Includes <strong>authentication</strong> and a <strong>light-weight image loader</strong> algorithm.",
     link: "https://github.com/muhhae/lorem-ipsum",
+    images: ["/screenshots/lorem-ipsum/1.jpg","/screenshots/lorem-ipsum/2.jpg","/screenshots/lorem-ipsum/3.jpg"],
     featured: false,
   },
 ];
