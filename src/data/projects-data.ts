8544448865d2cd692ac446b29cbbf73dfad43bae
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-12-27 14:03:47 +0700

    added png version

--- src/data/projects-data.ts
+++ src/data/projects-data.ts
@@ -1,6 +1,6 @@
 export interface Project {
   title: string;
-  description:string;
+  description: string;
   explanation?: string;
   tech?: string[];
   link?: string;
@@ -14,31 +14,39 @@ export interface Project {
 export const projectsData: Project[] = [
   {
     title: "Interpreted Programming Language",
-    description: "Implemented core programming language features like <strong>variables, arithmetic, functions, and classes</strong>. Designed and implemented a <strong>custom IDE</strong> with syntax highlighting and an interactive shell.",
+    description:
+      "Implemented core programming language features like <strong>variables, arithmetic, functions, and classes</strong>. Designed and implemented a <strong>custom IDE</strong> with syntax highlighting and an interactive shell.",
     link: "https://github.com/muhhae/interpreted-lang-py",
     images: ["/screenshots/pyhaekal.png"],
     featured: false,
   },
   {
     title: "Tetromino - Tetromania Castle",
-    description: "Implemented the <strong>game mechanics and 2D collision detection</strong> from scratch using C++.",
+    description:
+      "Implemented the <strong>game mechanics and 2D collision detection</strong> from scratch using C++.",
     link: "https://github.com/muhhae/tetromania-castle",
     web: "https://muhhae.itch.io/tetromania-castle",
-    images: ["/screenshots/tetromania.gif"],
+    images: ["/screenshots/tetromania.png", "/screenshots/tetromania.gif"],
     featured: false,
   },
   {
     title: "ESP32 PingPong Game",
-    description: "A Ping-Pong game written in C++ for ESP32 and a dot-matrix display. Features <strong>dot-matrix display rendering</strong> and a <strong>buzzer-based music player</strong>.",
+    description:
+      "A Ping-Pong game written in C++ for ESP32 and a dot-matrix display. Features <strong>dot-matrix display rendering</strong> and a <strong>buzzer-based music player</strong>.",
     link: "https://github.com/muhhae/esp32-pingpong",
     images: ["/screenshots/esp32pong.jpeg"],
     featured: false,
   },
   {
     title: "Image sharing platform - Lorem Ipsum",
-    description: "Developed a web application for sharing random images using Go and HTMX. Includes <strong>authentication</strong> and a <strong>light-weight image loader</strong> algorithm.",
+    description:
+      "Developed a web application for sharing random images using Go and HTMX. Includes <strong>authentication</strong> and a <strong>light-weight image loader</strong> algorithm.",
     link: "https://github.com/muhhae/lorem-ipsum",
-    images: ["/screenshots/lorem-ipsum/1.jpg","/screenshots/lorem-ipsum/2.jpg","/screenshots/lorem-ipsum/3.jpg"],
+    images: [
+      "/screenshots/lorem-ipsum/1.jpg",
+      "/screenshots/lorem-ipsum/2.jpg",
+      "/screenshots/lorem-ipsum/3.jpg",
+    ],
     featured: false,
   },
 ];
