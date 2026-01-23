d57c675e00ae19a38b2f747111f08dc27cc20367
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2026-01-23 21:22:51 +0700

    style: shorten my last name

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -98,7 +98,7 @@ export const translations: Record<string, Translations> = {
 
     // Hero Section (Main)
     heroGreeting: "$ whoami",
-    heroTitle: "Muhammad Haekal Muhyidin Al-Araby", // Change this to your real name
+    heroTitle: "Muhammad Haekal M. A.", // Change this to your real name
     heroSubtitle: "Undergraduate Researcher", // E.g: "Full Stack Developer", "Frontend Engineer", "Data Scientist"
     heroDescription: "Senior undergraduate student at Sepuluh Nopember Institute of Technology with a strong interest in <strong>SYSTEM</strong> research, particularly in Storage Systems. Passionate about building efficient and robust systems.",
     heroCta: "cd ~/projects",
