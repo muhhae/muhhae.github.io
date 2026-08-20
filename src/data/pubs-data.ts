132d3227f42566f21673bf0ecac7aa7cfab0f2bb
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2026-08-20 09:18:26 +0700

    feat: remove in press and replace link with pdf

--- src/data/pubs-data.ts
+++ src/data/pubs-data.ts
@@ -17,7 +17,7 @@ export const pubsData: Publication[] = [
   {
     title: "Demystifying and Improving Lazy Promotion in Cache Eviction",
     description:
-      "Proceeding of the VLDB Endowment (VLDB'26), <em>In press</em>",
+      "Proceeding of the VLDB Endowment (VLDB'26)",
     author: [
       "Qinghan Chen",
       `${MY_AUTHOR_MARKER}Muhammad Haekal Muhyidin Al-Araby`,
@@ -30,6 +30,6 @@ export const pubsData: Publication[] = [
       "Cache eviction algorithms play a critical role in the performance of modern data systems, yet their scalability is often limited by the high computational overhead associated with object promotions. Lazy Promotion techniques have emerged as relaxations of traditional Least-Recently-Used (LRU) methods, designed to alleviate lock contention and increase throughput. This work uses production traces from real-world systems to benchmark five Lazy Promotion strategies: Probabilistic-LRU, Batch-LRU, Delay-LRU, FIFO-reinsertion, and Random-LRU. We evaluate these techniques across miss ratio, scalability, promotion count, and a novel metric called promotion efficiency, which measures the number of hits per promotion. Our results reveal that Delay-LRU and FIFO-reinsertion significantly improve promotion efficiency, whereas Batch-LRU and Probabilistic-LRU struggle to reduce promotions without significantly increasing miss ratio. Moreover, we uncover substantial optimization potential, showing that most cache promotions are unnecessary when equipped with oracle knowledge. To further reduce promotions in LRU, we propose two novel enhancements—Delayed FIFO-reinsertion (D-FR) and Age-Guided Eviction (AGE) that reduce promotions by 20—60% while achieving a similar or lower miss ratio.",
     artifact: "https://github.com/cacheMon/Lazy-Promotions/",
     featured: false, // true = appears as "PINNED" at the top
-    link: "https://juncheng.seas.harvard.edu/publication/vldb26_lazyPromotion.pdf",
+    pdf: "https://juncheng.seas.harvard.edu/publication/vldb26_lazyPromotion.pdf",
   },
 ];
